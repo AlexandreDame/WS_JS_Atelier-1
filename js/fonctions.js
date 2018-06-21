f74aa55d77fb9785176cc597f0946479ca50ea2f
AlexandreDame <40430309+AlexandreDame@users.noreply.github.com>
2018-06-21 12:28:36 +0200

    Add files via upload

---
+++ js/fonctions.js
@@ -0,0 +1,10 @@
+﻿var texte_origine = "";
+
+function show_questionnaire() {
+    $("#sectionTexte").hide();
+    $("#sectionQuestionnaire").show();
+}
+function show_texte() {
+    $("#sectionTexte").show();
+    $("#sectionQuestionnaire").hide();
+}
